e125f2c0df7917c562def556604b95cca0ef2585
H. P. Gross <hpgross@gmail.com>
2021-05-23 17:57:18 -0400

    Actually subitted the changes

---
+++ simon.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+from gpiozero import LED, Button
+from time import sleep
+
+blue_led = LED(2)
+blue_button = Button(3)
+green_led = LED(17)
+green_button = Button(27)
+red_led = LED(10)
+red_button = Button(9)
+yellow_led = LED(5)
+yellow_button = Button(6)
+white_led = LED(19)
+white_button = Button(26)
+
+print("Welcome to Simon!\n\nWatch the pattern of lights, then press the buttons in the order they lit up!")
+
+#section for lighting up the leds
+
+#section for the button pressing for input
+
+#section to check, then put on a light show if successful
