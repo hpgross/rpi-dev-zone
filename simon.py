3e3ffa688c804b94095c9a62a1dcb50f08b7317f
hpgross <hpgross@gmail.com>
2021-05-23 19:20:45 -0400

    Added a section to randomly pick an LED to Simon

--- simon.py
+++ simon.py
@@ -2,6 +2,7 @@
 
 from gpiozero import LED, Button
 from time import sleep
+import random
 
 blue_led = LED(2)
 blue_button = Button(3)
@@ -14,9 +15,16 @@ yellow_button = Button(6)
 white_led = LED(19)
 white_button = Button(26)
 
+led_list = [2,17,10,5,19]
+button_list = [3,27,9,6,26]
+
 print("Welcome to Simon!\n\nWatch the pattern of lights, then press the buttons in the order they lit up!")
 
 #section for lighting up the leds
+current_led = random.choice(led_list)
+LED(current_led).on()
+sleep(1)
+LED(current_led).off()
 
 #section for the button pressing for input
 
