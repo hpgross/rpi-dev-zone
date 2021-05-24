51a6f6e8b1e6111455c53ba8a9ae54c3fa2ecbdc
hpgross <hpgross@gmail.com>
2021-05-23 21:28:02 -0400

    Wrote most of the code for Simon

--- simon.py
+++ simon.py
@@ -1,31 +1,69 @@
 #!/usr/bin/env python3
 
-from gpiozero import LED, Button
+from gpiozero import LEDBoard, Button
 from time import sleep
-import random
+from random import choice
+from sys import exit
 
-blue_led = LED(18)
-blue_button = Button(3)
-green_led = LED(17)
-green_button = Button(27)
-red_led = LED(10)
-red_button = Button(9)
-yellow_led = LED(5)
-yellow_button = Button(6)
-white_led = LED(19)
-white_button = Button(26)
+#leds in order are blue, green, red, yellow, white
+leds = LEDBoard(18,17,10,5,19)
 
-led_list = [18,17,10,5,19]
-button_list = [3,27,9,6,26]
+blue = Button(3)
+green = Button(27)
+red = Button(9)
+yellow = Button(6)
+white = Button(26)
+
+lights = []
+inputs = []
+
+def add_input(btn):
+    inputs.append({
+        blue:0,
+        green:1,
+        yellow:2,
+        red:3,
+        white:4,
+    }[btn])
+
+def lightshow():
+    i = 0
+    while i < 20:
+        current_led = choice(list(range(1,5)))
+        leds[current_led].on()
+        sleep(0.1)
+        leds[current_led].off()
+        i += 1
 
 print("Welcome to Simon!\n\nWatch the pattern of lights, then press the buttons in the order they lit up!")
 
-#section for lighting up the leds
-current_led = random.choice(led_list)
-LED(current_led).on()
-sleep(1)
-LED(current_led).off()
+num_lights = 1
+while num_lights < 6:
+    #section for lighting up the leds
+    for past_light in lights:
+        leds[past_light].on()
+        sleep(1)
+        leds[past_light].off()
+        sleep(1)
+    current_led = choice(list(range(5)))
+    leds[current_led].on()
+    sleep(1)
+    leds[current_led].off()
+    lights.append(current_led)
 
-#section for the button pressing for input
+    #section for the button pressing for input
+    while len(inputs) < len(lights):
+        for button in (blue, green, red, yellow, white):
+            button.when_pressed = add_input
+    
+    #checking for correct inputs
+    if lights == inputs:
+        num_lights += 1
+        inputs = []
+    else:
+        print("You lose!")
+        exit()
 
 #section to check, then put on a light show if successful
+if lights == inputs:
+    lightshow()
